61d4dafc072a0274f303946cb4ff506c1700377f
unknown <kymohendriks@gmail.com>
2022-08-16 17:54:34 +0200

    yuh

--- src/main/java/net/supers/test/mod/SupersTestMod.java
+++ src/main/java/net/supers/test/mod/SupersTestMod.java
@@ -10,7 +10,7 @@ import net.minecraft.util.registry.Registry;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-public class ExampleMod implements ModInitializer {
+public class SupersTestMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.

890b8e9914e00b3fcac3e5cc29b20302d5b15616
unknown <kymohendriks@gmail.com>
2022-08-16 14:53:39 +0200

    yuhhh

--- src/main/java/net/supers/test/mod/ExampleMod.java
+++ src/main/java/net/supers/test/mod/ExampleMod.java
@@ -1,6 +1,12 @@
-package net.fabricmc.example;
+package net.supers.test.mod;
 
 import net.fabricmc.api.ModInitializer;
+import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -8,7 +14,8 @@ public class ExampleMod implements ModInitializer {
 	// This logger is used to write text to the console and the log file.
 	// It is considered best practice to use your mod id as the logger's name.
 	// That way, it's clear which mod wrote info, warnings, and errors.
-	public static final Logger LOGGER = LoggerFactory.getLogger("modid");
+	public static final Logger LOGGER = LoggerFactory.getLogger("superstestmod");
+    public static final Item SOMETHING = new Item(new FabricItemSettings().group(ItemGroup.MISC));
 
 	@Override
 	public void onInitialize() {
@@ -16,6 +23,6 @@ public class ExampleMod implements ModInitializer {
 		// However, some things (like resources) may still be uninitialized.
 		// Proceed with mild caution.
 
-		LOGGER.info("Hello Fabric world!");
-	}
+        Registry.register(Registry.ITEM, new Identifier("superstestmod", "something"), SOMETHING);
+    }
 }

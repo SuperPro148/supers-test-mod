ecabebe89cb65afdfac6e6bd7a9e77a797bf1c41
unknown <kymohendriks@gmail.com>
2022-08-19 17:49:45 +0200

    just fixed some things

--- src/main/java/net/supers/test/mod/SupersTestMod.java
+++ src/main/java/net/supers/test/mod/SupersTestMod.java
@@ -2,6 +2,7 @@ package net.supers.test.mod;
 
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
+import net.minecraft.item.FoodComponent;
 import net.minecraft.item.Item;
 import net.minecraft.item.ItemGroup;
 import net.minecraft.util.Identifier;
@@ -13,8 +14,8 @@ import org.slf4j.LoggerFactory;
 public class SupersTestMod implements ModInitializer {
 
 	public static final Logger LOGGER = LoggerFactory.getLogger("superstestmod");
-    public static final Item CHEESE_MAKER = new CheeseMaker(new FabricItemSettings().group(ItemGroup.TOOLS));
-    public static final Item CHEESE = new Item(new FabricItemSettings().group(ItemGroup.FOOD));
+    public static final Item CHEESE_MAKER = new CheeseMaker(new FabricItemSettings().group(ItemGroup.TOOLS).maxCount(1));
+    public static final Item CHEESE = new Item(new FabricItemSettings().group(ItemGroup.FOOD).food(new FoodComponent.Builder().hunger(4).saturationModifier(6).build()));
 
 	@Override
 	public void onInitialize() {

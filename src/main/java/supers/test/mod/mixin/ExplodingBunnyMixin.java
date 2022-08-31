f7eaf9e6cb2287ebddea807992758697312162fe
unknown <kymohendriks@gmail.com>
2022-08-31 21:01:49 +0200

    added explosive bunnies

---
+++ src/main/java/supers/test/mod/mixin/ExplodingBunnyMixin.java
@@ -0,0 +1,27 @@
+package supers.test.mod.mixin;
+
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.world.World;
+import net.minecraft.world.explosion.Explosion;
+
+@Mixin(LivingEntity.class)
+public abstract class ExplodingBunnyMixin extends Entity {
+    public ExplodingBunnyMixin(EntityType<?> type, World world) {
+        super(type, world);
+    }
+
+    @Inject(method = "onDeath", at = @At("TAIL"))
+    private void superstestmod$makeBunniesExplode(CallbackInfo ci) {
+        if (this.getType() == EntityType.RABBIT) {
+            Explosion.DestructionType destructionType = Explosion.DestructionType.DESTROY;
+            this.world.createExplosion(this, this.getX(), this.getY(), this.getZ(), 6.0f, destructionType);
+        }
+    }
+}
